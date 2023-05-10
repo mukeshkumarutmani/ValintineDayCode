53eb6a85763ad352f6068522b474618c70845f68
[Mukesh Kumar Utmani] <[kumarmukesh789123@gmail.com]>
2023-05-10 22:39:35 +0500

    main

---
+++ Array32.java
@@ -0,0 +1,13 @@
+public class Array32{
+    public static void main(String[] args){
+        int arr[]={2,3,5,7,8,7};
+        int sum=0;
+        for(int i=1; i<arr.length; i++){
+               sum +=arr[i];
+            }
+            System.out.println(sum);
+        
+    }
+}
+    
+
